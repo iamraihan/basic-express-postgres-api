c2c342f50bd08c5535f7307013d0b3bb866c14a2
Raihan Ali <trueraihan@gmail.com>
2025-04-05 11:33:56 +0600

    feat: toCamelCase made separate functions under utils

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -1,19 +1,10 @@
 const pool = require("../pool");
+const toCamelCase = require("./utils/toCamelCase");
 
 class UserRepo {
   static async find() {
     const { rows } = await pool.query('SELECT * FROM "users"');
-    const parsedRows = rows.map((row) => {
-      const replaced = {};
-      for (let key in row) {
-        const camelCase = key.replace(/([-_][a-z])/gi, ($1) =>
-          $1[1].toUpperCase().replace("_", "")
-        );
-        replaced[camelCase] = row[key];
-      }
-      return replaced;
-    });
-    return parsedRows;
+    return toCamelCase(rows);
   }
   static async findById() {}
   static async insert() {}
